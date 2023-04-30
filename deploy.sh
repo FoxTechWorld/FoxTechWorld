f68e5404b4a2dd9f16bdcf60d92126347aeaf82b
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2023-04-30 00:58:11 -0300

    no message for this commit

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+commit="no message for this commit"
+[[ "${1}" ]] && commit=${1}
+hugo
+
+function public_deploy(){
+  cd public
+  git add .
+  git commit -m "${commit}"
+  git push origin main
+}
+
+function repo_deploy(){
+  cd ..
+  git add .
+  git commit -m "$commit"
+  git push origin main
+}
+
+function deploy(){
+  public_deploy;
+  repo_deploy
+}
+
+function start(){
+  deploy
+}
+
+start
